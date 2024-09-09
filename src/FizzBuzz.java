613ecd2861d941cdca56347324289f33b9d59996
Valerie Kim <val.kim712@gmail.com>
2024-09-09 18:46:46 -0400

    fixed while loop and added main class

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -6,14 +6,16 @@ class FizzBuzz {
     public static void main(String[] args) {
 
         int i = 1;
-        while (i++ < 100) {
+        while (i < 100) {
 
-            // Find out which numbers divide i.
-            doFizzBuzz(i);
+            i = doFizzBuzz(i);
         }
     }
 
-    private static void doFizzBuzz(int i) {
+    public static int doFizzBuzz(int i) {
+        // Find out which numbers divide i.
+
+
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
 
@@ -35,5 +37,7 @@ class FizzBuzz {
             System.out.println(i);
 
         }
+        i++;
+        return i;
     }
 }
